0b2b871a10969832aab61e88d632aece839030cd
mitaly <senmitaly13@gmail.com>
2017-08-13 16:32:53 +0530

    complete project

--- app/src/main/java/com/techeducation/groupmanager/ConnectionUtil.java
+++ app/src/main/java/com/techeducation/groupmanager/ConnectionUtil.java
@@ -5,17 +5,16 @@ package com.techeducation.groupmanager;
  */
 
 public class ConnectionUtil {
-    public static final String PHP_REGISTER_URL = "http://yourtube.in/mitalyhead/register_user.php";
-    public static final String PHP_LOGIN_URL = "http://yourtube.in/mitalyhead/login.php";
-    public static final String PHP_LIST_OF_USERS_URL = "http://yourtube.in/mitalyhead/list_of_users.php";
-    public static final String PHP_VIEW_PROFILE_URL = "http://yourtube.in/mitalyhead/view_profile.php";
-    public static final String PHP_COUNT_USERS_URL = "http://yourtube.in/mitalyhead/count_of_users.php";
-    public static final String PHP_EDIT_PROFILE_URL = "http://yourtube.in/mitalyhead/edit_profile.php";
-    public static final String PHP_CHANGE_SUSPEND_URL = "http://yourtube.in/mitalyhead/change_suspend.php";
-    public static final String PHP_GET_SUSPEND_VALUE_URL = "http://yourtube.in/mitalyhead/get_suspend_value.php";
-    public static final String PHP_CHANGE_ACCESS_URL = "http://yourtube.in/mitalyhead/change_access.php";
-    public static final String PHP_GET_LOGIN_VALUES_URL = "http://yourtube.in/mitalyhead/get_login_values.php";
-    public static final String PHP_POST_EVENT_URL = "http://yourtube.in/mitalyhead/post_event.php";
-    public static final String PHP_FETCH_FEED = "http://yourtube.in/mitalyhead/fetch_feed.php";
-    public static final String PHP_FETCH_EVENTS_HISTORY = "http://yourtube.in/mitalyhead/fetch_events_history.php";
+    public static final String PHP_REGISTER_URL = "REGISTER URL";
+    public static final String PHP_LOGIN_URL = "LOGIN URL";
+    public static final String PHP_LIST_OF_USERS_URL = "LIST OF USERS URL";
+    public static final String PHP_VIEW_PROFILE_URL = "VIEW PROFILE URL";
+    public static final String PHP_COUNT_USERS_URL = "COUNT USERS URL";
+    public static final String PHP_EDIT_PROFILE_URL = "EDIT PROFILE URL";
+    public static final String PHP_CHANGE_SUSPEND_URL = "CHANGE SUSPEND URL";
+    public static final String PHP_CHANGE_ACCESS_URL = "CHANGE ACCESS URL";
+    public static final String PHP_GET_LOGIN_VALUES_URL = "GET LOGIN VALUES URL";
+    public static final String PHP_POST_EVENT_URL = "POST EVENT URL";
+    public static final String PHP_FETCH_FEED = "FETCH FEED URL";
+    public static final String PHP_FETCH_EVENTS_HISTORY = "FETCH EVENTS HISTORY URL";
 }
